15d21957a02e5a2147f8b2300d7e8955759fce84
wohenzhendeai <126291407+wozhendeai@users.noreply.github.com>
2025-09-18 17:31:48 -0400

    fix: tailwind config
    the default component styling was broken due to school wifi
    
    Co-authored-by: spollito <spollito@users.noreply.github.com>
    Co-authored-by: DanS-89 <DanS-89@users.noreply.github.com>
    Co-authored-by: Wieefi <Wieefi@users.noreply.github.com>
    Co-authored-by: yingxingxin <yingxingxin@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+// Root page for the app (/)
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button>hey</Button>
     </div>
   );
 }
